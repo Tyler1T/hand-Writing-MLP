9894336c1baeedbf00d38cafb54140f457ccf9e9
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-10-27 14:29:26 -0500

    Setting up import stuff

---
+++ trainer.py
@@ -0,0 +1,24 @@
+import pytorch
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+# number of subprocesses to use for data loading
+num_workers = 0
+# how many samples per batch to load
+batch_size = 20
+
+# convert data to torch.FloatTensor
+transform = transforms.ToTensor()
+
+# choose the training and test datasets
+train_data = datasets.MNIST(root='data', train=True,
+                                   download=True, transform=transform)
+test_data = datasets.MNIST(root='data', train=False,
+                                  download=True, transform=transform)
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
+    num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
+    num_workers=num_workers)
