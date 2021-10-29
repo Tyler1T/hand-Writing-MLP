12b4f5968ece56df0b50fc9535378da5c7f10702
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-10-29 10:40:02 -0500

    Got rid of plotting code

--- neuralTrainer.py
+++ neuralTrainer.py
@@ -25,37 +25,6 @@ train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size,
 test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size,
     num_workers=num_workers)
 
-import matplotlib.pyplot as plt
-
-# obtain one batch of training images
-dataiter = iter(train_loader)
-images, labels = dataiter.next()
-images = images.numpy()
-
-# plot the images in the batch, along with the corresponding labels
-fig = plt.figure(figsize=(25, 4))
-for idx in np.arange(batch_size):
-    ax = fig.add_subplot(2, int(batch_size/2), idx+1, xticks=[], yticks=[])
-    ax.imshow(np.squeeze(images[idx]), cmap='gray')
-    # print out the correct label for each image
-    # .item() gets the value contained in a Tensor
-    ax.set_title(str(labels[idx].item()))
-
-img = np.squeeze(images[1])
-
-fig = plt.figure(figsize = (12,12))
-ax = fig.add_subplot(111)
-ax.imshow(img, cmap='gray')
-width, height = img.shape
-thresh = img.max()/2.5
-for x in range(width):
-    for y in range(height):
-        val = round(img[x][y],2) if img[x][y] !=0 else 0
-        ax.annotate(str(val), xy=(y,x),
-                    horizontalalignment='center',
-                    verticalalignment='center',
-                    color='white' if img[x][y]<thresh else 'black')
-
 import torch.nn as nn
 import torch.nn.functional as F
 from torchsummary import summary
@@ -95,7 +64,7 @@ criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
 # number of epochs to train the model
-n_epochs = 50  # suggest training between 20-50 epochs
+n_epochs = 1  # suggest training between 20-50 epochs
 
 # Set model to the training mode
 model.train()
@@ -186,13 +155,6 @@ _, preds = torch.max(output, 1)
 # prep images for display
 images = images.numpy()
 
-# plot the images in the batch, along with predicted and true labels
-fig = plt.figure(figsize=(25, 4))
-for idx in np.arange(batch_size):
-    ax = fig.add_subplot(2, int(batch_size/2), idx+1, xticks=[], yticks=[])
-    ax.imshow(np.squeeze(images[idx]), cmap='gray')
-    ax.set_title("{} ({})".format(str(preds[idx].item()), str(labels[idx].item())),
-                 color=("green" if preds[idx]==labels[idx] else "red"))
 torch.save(model, 'model.pth')
 f = open("hiddenLayerValues.txt", "w")
 for name, param in model.named_parameters():
