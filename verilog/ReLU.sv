0eebf3e6dba5d7300d656346a6b82a3e5e3a73d4
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-12-05 17:24:46 -0600

    Fixed ReLU function

--- verilog/ReLU.sv
+++ verilog/ReLU.sv
@@ -2,8 +2,5 @@ module ReLU(in, out);
     input logic [15:0] in;
     output logic [15:0] out;
 
-    logic test;
-    assign test = in[15];
-
-    assign out[15:0] = test ? 16'b0 : in[15:0];
+    assign out[15:0] = (in[15] == 0) ? 16'b0 : in[15:0];
 endmodule
