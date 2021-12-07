5f0ec4c01939117b0389650f75fca62e847f6376
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-12-07 08:56:39 -0600

    Fixed script for writing network and put into network module

--- scripts/neuralCreationScript.py
+++ scripts/neuralCreationScript.py
@@ -1,6 +1,13 @@
 import os
 
 f = open("network.txt", "w")
+for i in range(0, 200):
+    f.write(f"neuron_layer1 n1_{i} (data, {i}, layer1_output[{i}]);\n")
 
+f.write("\n")
 for i in range(0, 50):
-    f.write(f"neuron_layer1 n{i} (data[767:0], {i});\n")
+    f.write(f"neuron_layer2 n2_{i} (layer1_output, {i}, layer2_output[{i}]);\n")
+
+f.write("\n")
+for i in range(0, 10):
+    f.write(f"neuron_layer3 n3_{i} (layer2_output, {i}, result[{i}]);\n")
