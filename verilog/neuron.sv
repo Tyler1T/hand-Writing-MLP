ef3c2fc926ce3d5f11fc9f712e7f11e15981a3d8
Tyler Tucker <tylermatthewtucker@gmail.com>
2021-12-07 09:01:50 -0600

    Fixed neurons to take in data properly

--- verilog/neuron.sv
+++ verilog/neuron.sv
@@ -1,7 +1,7 @@
 module neuron_layer1(data, neuron, out);
-    input logic [7:0] neuron;
-    input logic [783:0] data[15:0];
-    output logic [15:0] out;
+    input logic neuron [7:0];
+    input logic [0:15] data[783:0];
+    output logic out[15:0];
 
     logic [15:0] bias;
     logic [17:0] ra1;
@@ -17,7 +17,7 @@ module neuron_layer1(data, neuron, out);
         assign ra1 = {neuron, i};
         regfile_weight1 weight(ra1, weight);
         csam multiplier(temp, weight, data[i])
-        CLA_with_BCLG16 adder(temp, pre, 1'b0, sum);
+        CLA_with_BCLG16 adder(temp, sum, 1'b0, sum);
     }
 
     CLA_with_BCLG16 adder(sum, bias, 1'b0, to_ReLU);
@@ -25,11 +25,10 @@ module neuron_layer1(data, neuron, out);
     ReLU activation(to_ReLU, out);
 endmodule // 1st hidden layer neuron, 200 neurons
 
-module neuron_layer2(data, bias, neuron, out);
-    input logic [5:0] neuron;
-    input logic [199:0] data[15:0];
-    input logic [15:0] bias;
-    output logic [15:0] out;
+module neuron_layer2(data, neuron, out);
+    input logic neuron [5:0];
+    input logic [0:15] data[199:0];
+    output logic out[15:0];
 
     logic [17:0] ra1;
     logic [15:0] weight;
@@ -38,21 +37,20 @@ module neuron_layer2(data, bias, neuron, out);
 
     regfile_bias2 bias(neuron, bias);
 
-    for(int i = 0; i < 768; i++){
+    for(int i = 0; i < 200; i++){
         assign ra1 = {neuron, i};
         regfile_weight2 weight(ra1, weight);
         csam multiplier(temp, weight, data[i])
-        CLA_with_BCLG16 adder(temp, pre, 1'b0, sum);
+        CLA_with_BCLG16 adder(temp, sum, 1'b0, sum);
     }
 
     ReLU activation(sum, out);
 endmodule // 2nd hidden layer neuron, 50 neurons
 
-module neuron_layer3(data, bias, neuron, out);
-    input logic [3:0] neuron;
-    input logic [49:0] data[15:0];
-    input logic [15:0] bias;
-    output logic [15:0] out;
+module neuron_layer3(data, neuron, out);
+    input logic neuron [3:0];
+    input logic [0:15] data[49:0];
+    output logic out[15:0];
 
     logic [17:0] ra1;
     logic [15:0] weight;
@@ -61,11 +59,11 @@ module neuron_layer3(data, bias, neuron, out);
 
     regfile_bias3 bias(neuron, bias);
 
-    for(int i = 0; i < 768; i++){
+    for(int i = 0; i < 50; i++){
         assign ra1 = {neuron, i};
         regfile_weight3 weight(ra1, weight);
         csam multiplier(temp, weight, data[i])
-        CLA_with_BCLG16 adder(temp, pre, 1'b0, out);
+        CLA_with_BCLG16 adder(temp, sum, 1'b0, out);
     }
 
 endmodule // output layer neuron, 10 neurons
